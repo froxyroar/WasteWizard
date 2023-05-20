5585d04240f011115d318261137109d48bc5b292
froxyroar <rifkirosada7@gmail.com>
2023-05-20 14:03:53 +0700

    Adding dark mode part 2

--- app/src/main/java/com/kelompok3/wastewizard/HomeFragment.kt
+++ app/src/main/java/com/kelompok3/wastewizard/HomeFragment.kt
@@ -2,48 +2,47 @@ package com.kelompok3.wastewizard
 
 import android.content.Context
 import android.content.Intent
-import android.content.res.Configuration
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
+import androidx.fragment.app.Fragment
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
-
 class HomeFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var switch: SwitchCompat
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        switch = view.findViewById(R.id.switch_btn)
         val btnLogout = view.findViewById<Button>(R.id.btn_logout)
-        val switch = view.findViewById<SwitchCompat>(R.id.switch_btn)
-        val sharedPreferences = requireContext().getSharedPreferences("Mode", Context.MODE_PRIVATE)
+
+        sharedPreferences = requireContext().getSharedPreferences("Mode", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         val nightMode = sharedPreferences.getBoolean("night", false)
 
-        if(nightMode){
-            switch.isChecked = true
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        }
+        switch.isChecked = nightMode
 
-        switch.setOnCheckedChangeListener{ _, isChecked ->
-            if(!isChecked){
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (!isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                 editor.putBoolean("night", false)
                 editor.apply()
-            }else{
+            } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
                 editor.putBoolean("night", true)
                 editor.apply()
@@ -61,9 +60,11 @@ class HomeFragment : Fragment() {
             mGoogleSignInClient.signOut().addOnCompleteListener(requireActivity()) {
                 mGoogleSignInClient.revokeAccess().addOnCompleteListener(requireActivity()) {
                     startActivity(intent)
+                    requireActivity().finish() // Finish the current activity to prevent issues with the theme switch
                 }
             }
         }
+
         return view
     }
 }
